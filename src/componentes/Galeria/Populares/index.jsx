e1b53eb687a5e7cee5a1c4afea0432f935e52392
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-21 15:34:01 -0300

    estruturando a galeria

---
+++ src/componentes/Galeria/Populares/index.jsx
@@ -0,0 +1,7 @@
+import TituloEstilizado from "../../Titulo";
+
+const Populares = () => {
+  return <TituloEstilizado $alinhamento="center">Populares</TituloEstilizado>;
+};
+
+export default Populares;
--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,13 +1,28 @@
+import styled from "styled-components";
 import TituloEstilizado from "../Titulo";
+import Populares from "./Populares";
 import Tags from "./Tags";
+
+const GaleriaContainer = styled.div`
+  display: flex;
+`;
+
+const SessaoFluidaGaleria = styled.section`
+  flex-grow: 1;
+`;
 
 const Galeria = () => {
   return (
     <>
       <Tags />
-      <TituloEstilizado>
-        Navegue pela galeria <span>🚀 🛰️</span>
-      </TituloEstilizado>
+      <GaleriaContainer>
+        <SessaoFluidaGaleria>
+          <TituloEstilizado>
+            Navegue pela galeria <span>🚀 🛰️</span>
+          </TituloEstilizado>
+        </SessaoFluidaGaleria>
+        <Populares />
+      </GaleriaContainer>
     </>
   );
 };
