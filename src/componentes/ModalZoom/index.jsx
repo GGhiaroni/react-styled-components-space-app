ddc4cde8803ada145d176564e0fc1de82518af8e
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-22 19:30:30 -0300

    fazendo a modal abrir a foto correta quando clicada

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,27 +1,24 @@
+import Imagem from "../Galeria/Imagem";
+
 const ModalZoom = ({ foto }) => {
   return (
     <>
-      <dialog open={!!foto} id="favDialog">
-        <form method="dialog">
-          <section>
-            <p>
-              <label for="favAnimal">Favorite animal:</label>
-              <select id="favAnimal">
-                <option></option>
-                <option>Brine shrimp</option>
-                <option>Red panda</option>
-                <option>Spider monkey</option>
-              </select>
-            </p>
-          </section>
-          <menu>
-            <button id="cancel" type="reset">
-              Cancel
-            </button>
-            <button type="submit">Confirm</button>
-          </menu>
-        </form>
-      </dialog>
+      {foto && (
+        <>
+          <dialog open={!!foto}>
+            <Imagem
+              src={foto.path}
+              titulo={foto.titulo}
+              fonte={foto.fonte}
+              id={foto.id}
+              expandida={true}
+            />
+            <form method="dialog">
+              <button formMethod="dialog"></button>
+            </form>
+          </dialog>
+        </>
+      )}
     </>
   );
 };
