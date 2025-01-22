8aa0029ef2cc005714d04c7ec7ab6202623b5de8
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-22 08:53:05 -0300

    criando a estrutura da tag e consumindo o arquivo json

--- src/componentes/Galeria/Tags/index.jsx
+++ src/componentes/Galeria/Tags/index.jsx
@@ -1,5 +1,14 @@
+import tags from "./tags.json";
+
 const Tags = () => {
-  return <h2>Tags.....</h2>;
+  return (
+    <>
+      <p>Busque por tags:</p>
+      {tags.map((tag) => (
+        <button key={tag.id}>{tag.titulo}</button>
+      ))}
+    </>
+  );
 };
 
 export default Tags;
