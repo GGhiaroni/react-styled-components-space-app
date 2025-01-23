f04aa15d064410bc9e3037d70ee5bd18d59df612
GGhiaroni <gghiaronitiziano@gmail.com>
2025-01-23 07:57:34 -0300

    criacao do overlay

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,10 +1,21 @@
+import styled from "styled-components";
 import Imagem from "../Galeria/Imagem";
+
+const Overlay = styled.div`
+  background-color: rgba(0, 0, 0, 0.7);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
 
 const ModalZoom = ({ foto }) => {
   return (
     <>
       {foto && (
         <>
+          <Overlay />
           <dialog open={!!foto}>
             <Imagem
               src={foto.path}
